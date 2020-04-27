70c7122d25fc43cf03c6426111ca77cdade6bced
Tyler McGinnis <tylermcginnis33@gmail.com>
2020-04-26 22:46:12 -0600

    Code for 6-fetch-data

--- src/server/index.js
+++ src/server/index.js
@@ -5,6 +5,8 @@ import * as React from 'react'
 import App from '../shared/App'
 import serialize from "serialize-javascript"
 import { fetchPopularRepos } from '../shared/api'
+import routes from '../shared/routes'
+import { matchPath } from 'react-router-dom'
 
 const app = express()
 
@@ -12,28 +14,33 @@ app.use(cors())
 app.use(express.static("dist"))
 
 app.get("*", (req, res, next) => {
-  fetchPopularRepos()
-    .then((data) => {
-      const markup = ReactDOM.renderToString(
-        <App data={data} />
-      )
+  const activeRoute = routes.find((route) => matchPath(req.url, route)) || {}
 
-      res.send(`
-        <!DOCTYPE html>
-        <html>
-          <head>
-            <title>SSR with RRv5</title>
-            <script src="/bundle.js" defer></script>
-            <link href="/main.css" rel="stylesheet">
-            <script>window.__INITIAL_DATA__ = ${serialize(data)}</script>
-          </head>
+  const promise = activeRoute.fetchInitialData
+    ? activeRoute.fetchInitialData(req.path)
+    : Promise.resolve()
 
-          <body>
-            <div id="app">${markup}</div>
-          </body>
-        </html>
-      `)
-    })
+  promise.then((data) => {
+    const markup = ReactDOM.renderToString(
+      <App data={data} />
+    )
+
+    res.send(`
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>SSR with RRv5</title>
+          <script src="/bundle.js" defer></script>
+          <link href="/main.css" rel="stylesheet">
+          <script>window.__INITIAL_DATA__ = ${serialize(data)}</script>
+        </head>
+
+        <body>
+          <div id="app">${markup}</div>
+        </body>
+      </html>
+    `)
+  }).catch(next)
 })
 
 const PORT = process.env.PORT || 3000
